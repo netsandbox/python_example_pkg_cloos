8a162e416a196bf3460701802c975ef9b7ca83ca
Christian Loos <cloos@netsandbox.de>
2019-01-20 19:03:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# example_pkg_cloos
+
+A example package to learn and test Python packaging.
+
+https://packaging.python.org/tutorials/packaging-projects/
+
+## Development workflow
+
+Checkout git repository:
+
+```shell
+git clone git@github.com:cloos/python_example_pkg_cloos.git
+```
+
+Create virtualenv:
+
+```shell
+make venv
+```
+
+Activate virtualenv:
+
+```shell
+source venv/bin/activate
+```
+
+Install/Upgrade packaging tools:
+
+```shell
+make install
+```
+
+Install package in 'development mode'
+
+```shell
+make develop
+```
+
+Upload to https://test.pypi.org/:
+
+```shell
+make upload_test
+```
+
+Upload to https://pypi.org/:
+
+```shell
+make upload
+```
+
+## Usage
+
+### Cli
+
+```shell
+example-pkg-cloos --help
+```
+
+### Library
+
+```python
+from example_pkg_cloos.utils import print_bar, print_foo
+
+print_bar()
+print_foo()
+```
---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+from example_pkg_cloos import __version__
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="example_pkg_cloos",
+    version=__version__,
+    author="Christian Loos",
+    author_email="cloos@netsandbox.de",
+    description="A example package to learn and test Python packaging.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/cloos/python_example_pkg_cloos",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=[
+        "click",
+    ],
+    entry_points={
+        "console_scripts": "example-pkg-cloos=example_pkg_cloos.cli:main"
+    },
+)
