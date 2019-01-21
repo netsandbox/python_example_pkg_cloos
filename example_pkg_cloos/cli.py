900e081221ab1c1e10fb87ba9a42a2706b389b42
Christian Loos <cloos@netsandbox.de>
2019-01-21 23:33:17 +0100

    add version option

--- example_pkg_cloos/cli.py
+++ example_pkg_cloos/cli.py
@@ -13,6 +13,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option("-b", "--bold", is_flag=True, help="Print text in bold?")
 @click.option("-c", "--color", required=True, type=click.Choice(["green", "yellow", "red"]), help="Text color.")
 @click.option("-t", "--text", required=True, help="Text to print.")
+@click.version_option()
 def main(bold, color, text):
     """ print text in color and optionally in bold """
 
