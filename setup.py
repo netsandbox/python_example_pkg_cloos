1448e255b81c5cba666488b0d8cf0ad41799db7a
Christian Loos <cloos@netsandbox.de>
2019-02-13 19:13:12 +0100

    test more python versions

--- setup.py
+++ setup.py
@@ -24,8 +24,12 @@ setuptools.setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     setup_requires=["setuptools_scm"],
     use_scm_version=True,
     install_requires=[
