2d3259519213d451d616e7edecfb6aa6c373d33d
Christian Loos <cloos@netsandbox.de>
2019-10-16 19:03:34 +0200

    fix a problem detected by black

--- example_pkg_cloos/cli.py
+++ example_pkg_cloos/cli.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division, print_function
 
 import click
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
