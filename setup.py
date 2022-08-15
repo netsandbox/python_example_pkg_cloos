aceb66b233be2c12f56acf0a56e2abff3bf55e01
Christian Loos <cloos@netsandbox.de>
2022-08-15 13:59:15 +0200

    drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -34,7 +33,7 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Testing",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     setup_requires=["setuptools_scm"],
     use_scm_version=True,
     install_requires=[
